d49cb21921d931329c9133fa7250ed8734c1790b
thomasdecactus <thomaszijl@live.nl>
2020-05-18 17:38:30 +0200

    update

--- src/main/java/nl/Ipsen5Server/Main/Main.java
+++ src/main/java/nl/Ipsen5Server/Main/Main.java
@@ -6,40 +6,44 @@ import java.util.EnumSet;
 import javax.servlet.DispatcherType;
 import javax.servlet.FilterRegistration;
 
+import io.dropwizard.jdbi3.JdbiFactory;
+import io.dropwizard.server.DefaultServerFactory;
 import org.eclipse.jetty.servlets.CrossOriginFilter;
 
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.jdbi.v3.core.Jdbi;
 
 public class Main extends Application<Configuration>{
 	
 	
-		  public static void main(String[] args) throws Exception {
-			  new Main().run(new String[] { "server" });
-		  }
+    public static void main(String[] args) throws Exception {
+        new Main().run(new String[] { "server" });
+    }
 	
-	
-    /**
-     * @author Anthony Scheeres
-     */
+
     @Override
     public void initialize(Bootstrap<Configuration> bootstrap) {
        
     }
     
-    
-    /**
-     * @author Anthony Scheeres
-     */
+
     @Override
     public void run(Configuration configuration, Environment environment) throws Exception {
-    
-    	
+
+        //standard initalaizations
+        final JdbiFactory factory = new JdbiFactory();
+        final DefaultServerFactory serverFactory = (DefaultServerFactory) configuration.getServerFactory();
+        final Jdbi jdbi = factory.build(environment, configuration.getDataSourceFactory(), "postgresql");
+
+        //api prefix
+        serverFactory.setApplicationContextPath("/");
+        serverFactory.setJerseyRootPath("/api");
+
     	 // Enable CORS headers
-        final FilterRegistration.Dynamic cors =
-            environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
         
         // Configure CORS parameters
         cors.setInitParameter("allowedOrigins", "*");
