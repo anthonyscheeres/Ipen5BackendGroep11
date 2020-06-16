0d5b406820bd5c0805c59e605a471f31d94a52c3
Anthony <info@anthonyscheeres.nl>
2020-06-16 12:01:22 +0200

    finished

--- src/main/java/nl/Ipsen5Server/Presentation/UserResource.java
+++ src/main/java/nl/Ipsen5Server/Presentation/UserResource.java
@@ -18,314 +18,306 @@ import java.util.*;
 @Path("/user")
 public class UserResource {
 
-    private UserDAO dao;
-    private Authorisation tokenUtils;
-    private String failedResponeMessage = "Login credentials were invalide";
-    private Response defaultRespone = Response.serverError()
-        .entity(failedResponeMessage)
-        .build();
+ private UserDAO dao;
+ private Authorisation tokenUtils;
+ private String failedResponeMessage = "Login credentials were invalide";
+ private Response defaultRespone = Response.serverError()
+  .entity(failedResponeMessage)
+  .build();
 
 
-    public UserResource(UserDAO dao, Authorisation tokenUtils) {
-        super();
-        this.dao = dao;
-        this.tokenUtils = tokenUtils;
-    }
+ public UserResource(UserDAO dao, Authorisation tokenUtils) {
+  super();
+  this.dao = dao;
+  this.tokenUtils = tokenUtils;
+ }
 
 
-    /* @GET
-     public List < User > getAll() {
-      return dao.getAll();
-     }
-    */
+/* @GET
+ public List < User > getAll() {
+  return dao.getAll();
+ }
+*/
 
+ 
+ 
+ @Path("/{token}")
+ @GET
+ @Produces(MediaType.APPLICATION_JSON)
+ public Response getAllUsers( @PathParam("token") String token) {
+ 
+	 
+	 try {
 
 
-    @Path("/{token}")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getAllUsers(@PathParam("token") String token) {
 
+		   String Email2 = "Email";
+		   String UserPassword = "UserPassword";
 
-        try {
+		
 
 
+		   Map < String, String > credentials = tokenUtils.decrypt(token);
 
-            String Email2 = "Email";
-            String UserPassword = "UserPassword";
 
+		   String Email3 = credentials.get(Email2);
+		   String UserPassword2 = credentials.get(UserPassword);
 
+		   tokenUtils.check(new Account(Email3, UserPassword2), dao);
+	  
+	 
+	 ArrayList < User > allUsers = dao.getAll();
 
+  
+  
+  if (allUsers == null) {
+   throw new WebApplicationException(Response.Status.NOT_FOUND);
+  }
 
-            Map < String, String > credentials = tokenUtils.decrypt(token);
+  return Response.ok(allUsers, MediaType.APPLICATION_JSON).build();
+  
+  
+	  } catch (Exception e) {
 
+	  }
 
-            String Email3 = credentials.get(Email2);
-            String UserPassword2 = credentials.get(UserPassword);
+	  return defaultRespone ;
+  
+ }
 
-            tokenUtils.check(new Account(Email3, UserPassword2), dao);
 
+ @GET
+ @Produces(MediaType.APPLICATION_JSON)
+ @Path("/{token}/id/{Email}")
+ public Response findUserById(
+		 @PathParam("token") String token,
+		 @PathParam("Email") String Email) {
+ 
 
-            ArrayList < User > allUsers = dao.getAll();
+  
+  try {
 
 
 
-            if (allUsers == null) {
-                throw new WebApplicationException(Response.Status.NOT_FOUND);
-            }
+	   String Email2 = "Email";
+	   String UserPassword = "UserPassword";
 
-            return Response.ok(allUsers, MediaType.APPLICATION_JSON).build();
+	
 
 
-        } catch (Exception e) {
+	   Map < String, String > credentials = tokenUtils.decrypt(token);
 
-        }
 
-        return defaultRespone;
+	   String Email3 = credentials.get(Email2);
+	   String UserPassword2 = credentials.get(UserPassword);
 
-    }
+	   tokenUtils.check(new Account(Email3, UserPassword2), dao);
+  
+		 User user = dao.getUserByEmail(Email);
+		  if (user == null) {
+		   throw new WebApplicationException(Response.Status.NOT_FOUND);
+		  }
+	  
+	   return Response.ok(user, MediaType.APPLICATION_JSON).build();
+  
+  } catch (Exception e) {
 
+  }
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/{token}/id/{Email}")
-    public Response findUserById(
-        @PathParam("token") String token,
-        @PathParam("Email") String Email) {
+  return defaultRespone ;
+  
+  
+ }
 
 
+ @POST
+ @Path("/{token}/delete")
+ public Response deleteUser(
+		 @PathParam("token") String token,
+  @FormParam("Email") String Email
+ ) {
+	 
+	 
+	 try {
 
-        try {
 
 
+		   String Email2 = "Email";
+		   String UserPassword = "UserPassword";
 
-            String Email2 = "Email";
-            String UserPassword = "UserPassword";
+		
 
 
+		   Map < String, String > credentials = tokenUtils.decrypt(token);
 
 
-            Map < String, String > credentials = tokenUtils.decrypt(token);
+		   String Email3 = credentials.get(Email2);
+		   String UserPassword2 = credentials.get(UserPassword);
 
+		   tokenUtils.check(new Account(Email3, UserPassword2), dao);
+  dao.deleteByEmail(Email);
+  
 
-            String Email3 = credentials.get(Email2);
-            String UserPassword2 = credentials.get(UserPassword);
+  return Response.ok()
+   .entity("Deleted account with email: ' " + Email + " '  successfully")
+   .build();
+	 } catch (NotAuthorizedException e) {
 
-            tokenUtils.check(new Account(Email3, UserPassword2), dao);
+	  }
+	 return defaultRespone;
 
-            User user = dao.getUserByEmail(Email);
-            if (user == null) {
-                throw new WebApplicationException(Response.Status.NOT_FOUND);
-            }
+ }
 
-            return Response.ok(user, MediaType.APPLICATION_JSON).build();
+ @POST
+ @Path("/{token}/update")
+ public Response updateUser(
+		  @PathParam("token") String token,
+  @FormParam("UserID") String UserID,
+  @FormParam("ContactName") String ContactName,
+  @FormParam("CustomMessage") String CustomMessage,
+  @FormParam("Info") String Info
 
-        } catch (Exception e) {
+ ) {
+	 
+	 
+	 try {
 
-        }
 
-        return defaultRespone;
 
+		   String Email = "Email";
+		   String UserPassword = "UserPassword";
 
-    }
+		
 
 
-    @POST
-    @Path("/{token}/delete")
-    public Response deleteUser(
-        @PathParam("token") String token,
-        @FormParam("Email") String Email
-    ) {
+		   Map < String, String > credentials = tokenUtils.decrypt(token);
 
 
-        try {
+		   String Email2 = credentials.get(Email);
+		   String UserPassword2 = credentials.get(UserPassword);
 
+		   tokenUtils.check(new Account(Email2, UserPassword2), dao);
 
+	 
+	 
+  dao.updateByUsername(UserID, ContactName, CustomMessage, Info);
 
-            String Email2 = "Email";
-            String UserPassword = "UserPassword";
+  
+  
+  
+  return Response.ok()
+   .entity("Updated Account with userid: ' " + UserID + " '  successfully")
+   .build();
+  
+  
+	 } catch (NotAuthorizedException e) {
 
+	  }
+	 return defaultRespone;
+  
+ }
 
 
 
-            Map < String, String > credentials = tokenUtils.decrypt(token);
+ @PUT
+ @Path("/{token}/password")
+ public Response passwordUser(
 
+  @PathParam("token") String token,
+  Account user2
 
-            String Email3 = credentials.get(Email2);
-            String UserPassword2 = credentials.get(UserPassword);
+ ) {
+  Response response = defaultRespone;
 
-            tokenUtils.check(new Account(Email3, UserPassword2), dao);
-            dao.deleteByEmail(Email);
 
+  try {
 
-            return Response.ok()
-                .entity("Deleted account with email: ' " + Email + " '  successfully")
-                .build();
-        } catch (NotAuthorizedException e) {
 
-        }
-        return defaultRespone;
 
-    }
+   String Email = "Email";
+   String UserPassword = "UserPassword";
 
-    @POST
-    @Path("/{token}/update")
-    public Response updateUser(
-        @PathParam("token") String token,
-        @FormParam("UserID") String UserID,
-        @FormParam("ContactName") String ContactName,
-        @FormParam("CustomMessage") String CustomMessage,
-        @FormParam("Info") String Info
+   String NewPassword = user2.getUserPassword();
 
-    ) {
 
+   Map < String, String > credentials = tokenUtils.decrypt(token);
 
-        try {
 
+   String Email2 = credentials.get(Email);
+   String UserPassword2 = credentials.get(UserPassword);
 
+   tokenUtils.check(new Account(Email2, UserPassword2), dao);
 
-            String Email = "Email";
-            String UserPassword = "UserPassword";
+   new Thread(() -> {
 
+    dao.changePassword(Email2, UserPassword2, NewPassword);
 
+   }).start();
 
+   String message = "Successfull";
 
-            Map < String, String > credentials = tokenUtils.decrypt(token);
+   Response successResponse = Response.ok(message)
+    .build();
 
+   response = successResponse; //change response 
 
-            String Email2 = credentials.get(Email);
-            String UserPassword2 = credentials.get(UserPassword);
 
-            tokenUtils.check(new Account(Email2, UserPassword2), dao);
+  } catch (NotAuthorizedException e) {
 
+  }
 
 
-            dao.updateByUsername(UserID, ContactName, CustomMessage, Info);
+  return response;
 
 
 
 
-            return Response.ok()
-                .entity("Updated Account with userid: ' " + UserID + " '  successfully")
-                .build();
+ }
 
 
-        } catch (NotAuthorizedException e) {
 
-        }
-        return defaultRespone;
+ /**
+  *
+  * @author Anthony Scheeres
+  *
+  */
+ @POST
+ @Path("/login")
+ @Consumes(MediaType.APPLICATION_JSON)
+ public Response loginUser(
+  Account user
+ ) {
+  //define default response return when error
+  String failedResponeMessage = "Login credentials were invalide";
 
-    }
+  Response defaultRespone = Response.serverError()
+   .entity(failedResponeMessage)
+   .build();
 
+  Response response = defaultRespone; //return this response unless changed
 
+  try {
 
-    @PUT
-    @Path("/{token}/password")
-    public Response passwordUser(
+   tokenUtils.check(user, dao);
 
-        @PathParam("token") String token,
-        Account user2
+   String token = tokenUtils.create(user);
 
-    ) {
-        Response response = defaultRespone;
+   Response successResponse = Response.ok(new TokenBody(token), MediaType.APPLICATION_JSON) //Initialize success response and pass the token
 
+    .build();
 
-        try {
+   response = successResponse; //change response 
 
+  } catch (NotAuthorizedException e) {
+   //do something different or customize the response 
+  }
 
 
-            String Email = "Email";
-            String UserPassword = "UserPassword";
 
-            String NewPassword = user2.getUserPassword();
 
 
-            Map < String, String > credentials = tokenUtils.decrypt(token);
 
-
-            String Email2 = credentials.get(Email);
-            String UserPassword2 = credentials.get(UserPassword);
-
-            tokenUtils.check(new Account(Email2, UserPassword2), dao);
-
-            new Thread(() - > {
-
-                dao.changePassword(Email2, UserPassword2, NewPassword);
-
-            }).start();
-
-            String message = "Successfull";
-
-            Response successResponse = Response.ok(message)
-                .build();
-
-            response = successResponse; //change response 
-
-
-        } catch (NotAuthorizedException e) {
-
-        }
-
-
-        return response;
-
-
-
-
-    }
-
-
-    /**
-     *
-     * @author Anthony Scheeres
-     *
-     */
-
-
-
-
-    /**
-     *
-     * @author Anthony Scheeres
-     *
-     */
-    @POST
-    @Path("/login")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response loginUser(
-        Account user
-    ) {
-        //define default response return when error
-        String failedResponeMessage = "Login credentials were invalide";
-
-        Response defaultRespone = Response.serverError()
-            .entity(failedResponeMessage)
-            .build();
-
-        Response response = defaultRespone; //return this response unless changed
-
-        try {
-
-            tokenUtils.check(user, dao);
-
-            String token = tokenUtils.create(user);
-
-            Response successResponse = Response.ok(new TokenBody(token), MediaType.APPLICATION_JSON) //Initialize success response and pass the token
-
-                .build();
-
-            response = successResponse; //change response 
-
-        } catch (NotAuthorizedException e) {
-            //do something different or customize the response 
-        }
-
-
-
-
-
-
-        return response;
-    }
+  return response;
+ }
 
 }
