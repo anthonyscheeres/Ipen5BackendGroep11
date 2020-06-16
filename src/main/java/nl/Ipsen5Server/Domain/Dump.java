8ea5faf0f492c130ee994c1e51c752d32d8e25b6
Anthony <info@anthonyscheeres.nl>
2020-06-16 15:27:54 +0200

    directly added setters in dump model

--- src/main/java/nl/Ipsen5Server/Domain/Dump.java
+++ src/main/java/nl/Ipsen5Server/Domain/Dump.java
@@ -6,7 +6,7 @@ package nl.Ipsen5Server.Domain;
 */
 public class Dump {
 
-	private String user;
+	private String user; 
 	private String partial_IP;	
 	private String genoemde_social_media;
 	private String message;
@@ -23,18 +23,35 @@ public class Dump {
 		return user;
 	}
 
+	public void setUser(String user) {
+		this.user = user;
+	}
+
 	public String getPartial_IP() {
 		return partial_IP;
+	}
+
+	public void setPartial_IP(String partial_IP) {
+		this.partial_IP = partial_IP;
 	}
 
 	public String getGenoemde_social_media() {
 		return genoemde_social_media;
 	}
 
+	public void setGenoemde_social_media(String genoemde_social_media) {
+		this.genoemde_social_media = genoemde_social_media;
+	}
+
 	public String getMessage() {
 		return message;
 	}
-	
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
 
 
 	
