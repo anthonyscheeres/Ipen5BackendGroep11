c9acdf32e9a7c523084c6f92073517020c3814b2
Anthony <info@anthonyscheeres.nl>
2020-06-16 18:36:33 +0200

    added the empty fields that are present in the excel

--- src/main/java/nl/Ipsen5Server/Domain/Dump.java
+++ src/main/java/nl/Ipsen5Server/Domain/Dump.java
@@ -9,6 +9,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 */
 public class Dump {
 
+	
+	@JsonProperty("UID")
+	public String UID; 
+	
 	@JsonProperty("user")
 	public String user; 
 	
@@ -21,21 +25,111 @@ public class Dump {
 	@JsonProperty("message")
 	public String message;
 
-	public Dump(String user, String partial_IP, String genoemde_social_media, String message) {
-		super();
-		this.user = user;
-		
+	@JsonProperty("email")
+public String email;
 	
+	@JsonProperty("title")
+	public String title ;
+	
+	@JsonProperty("date")
+	public String date;
+	
+	@JsonProperty("site")
+	public String site;
+
+
+
+
+	public Dump(String uID, String user, String partial_IP, String genoemde_social_media, String message, String email,
+			String title, String date, String site) {
+		super();
+		UID = uID;
+		this.user = user;
 		this.partial_IP = partial_IP;
 		this.genoemde_social_media = genoemde_social_media;
 		this.message = message;
+		this.email = email;
+		this.title = title;
+		this.date = date;
+		this.site = site;
 	}
 
-	
-	
-	
+
+
+
 	public Dump() {
 		super();
+	}
+
+
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+
+	@JsonProperty("title")
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+
+
+	public String getDate() {
+		return date;
+	}
+
+
+
+	@JsonProperty("date")
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+
+
+
+	public String getSite() {
+		return site;
+	}
+
+
+
+	@JsonProperty("site")
+	public void setSite(String site) {
+		this.site = site;
+	}
+
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	@JsonProperty("email")
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+
+	public String getUID() {
+		return UID;
+	}
+
+
+
+
+	@JsonProperty("UID")
+	public void setUID(String uID) {
+		UID = uID;
 	}
 
 
